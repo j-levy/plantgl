492f9dbdeeb75be367763ea7aac3ae4491439fce
Jonathan LEVY <23365238+j-levy@users.noreply.github.com>
2021-06-10 21:51:57 +0200

    added objloader as 'editor' class

---
+++ src/openalea/plantgl/gui/objloader.py
@@ -0,0 +1,60 @@
+from PyQGLViewer import *
+from openalea.plantgl.scenegraph import *
+from openalea.plantgl.algo import *
+from openalea.plantgl.math import *
+from OpenGL.GL import *
+from openalea.plantgl.gui.qt import QtCore, QtGui
+import math
+
+from openalea.plantgl.gui.qt import QtCore, QtGui, QtOpenGL
+from openalea.plantgl.gui.qt.QtCore import QEvent, QObject, QPoint, Qt, pyqtSignal, qWarning
+from openalea.plantgl.gui.qt.QtGui import QColor, QImage,QFont
+from openalea.plantgl.gui.qt.QtOpenGL import QGLWidget
+from openalea.plantgl.gui.qt.QtWidgets import QFileDialog, QApplication
+
+from openalea.plantgl.gui.pglviewer import PglViewer
+import openalea.plantgl.scenegraph._pglsg as sg
+from openalea.plantgl.codec.obj import codec as obj_codec
+
+class ObjLoader (PglViewer):
+    BLACK_THEME = {'Curve' : (255,255,255), 'BackGround' : (51,51,51), 'Text' : (255,255,255), 'CtrlCurve' : (122,122,0), 'GridStrong' : (102,102,102), 'GridFade' : (51,51,51) , 'Points' : (250,30,30), 'FirstPoint' : (250,30,250), 'SelectedPoint' : (30,250,30), 'DisabledBackGround' : (150,150,150) }
+    WHITE_THEME = {'Curve' : (255,0,0), 'BackGround' : (255,255,255), 'Text' : (0,0,0), 'CtrlCurve' : (25,0,25), 'GridStrong' : (102,102,102), 'GridFade' : (153,153,153) , 'Points' : (30,250,30), 'FirstPoint' : (250,30,250), 'SelectedPoint' : (30,250,30), 'DisabledBackGround' : (150,150,150)}
+    valueChanged = QtCore.pyqtSignal()
+
+    def __init__(self,parent):
+        PglViewer.__init__(self,parent)
+
+    def loadFile(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', 
+            '~',"Obj files (*.obj)")[0]
+        scene = sg.Scene()
+        print(fname)
+        scene = obj_codec.read(fname)
+        self.display(scene)
+
+
+if __name__ == '__main__':
+    qapp = QtGui.QApplication([])
+    mv = ObjLoader(parent=None)
+    mv.setEnabled(True)
+    # mv.loadFile()
+
+    # list of points
+    vertices = [(1, 0, -1 / math.sqrt(2)),
+                (-1, 0, -1 / math.sqrt(2)),
+                (0, 1, 1 / math.sqrt(2)),
+                (0, -1, 1 / math.sqrt(2))]
+
+    # list of indices to make the faces
+    connectivity = [(0, 1, 2),
+                    (0, 1, 3),
+                    (1, 2, 3),
+                    (0, 2, 3)]  #
+    from openalea.plantgl.codec.obj import Group
+    g = Group("tetra")
+    s = g.shape(vertices=vertices, normals=connectivity, textures=[])
+    scene = sg.Scene()
+    scene.add(s)
+    mv.loadFile()
+    mv.show()
+    qapp.exec_()
